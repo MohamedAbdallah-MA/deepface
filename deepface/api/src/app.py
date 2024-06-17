626b84d9a6d8c61b2e334f533d05c6f3b65bbb76
Mohamed Abdallah <mohamed.abdallah5224@gmail.com>
2024-06-18 01:43:23 +0300

    make the default port integer

--- deepface/api/src/app.py
+++ deepface/api/src/app.py
@@ -14,9 +14,9 @@ def create_online_app():
     app = Flask(__name__)
     app.register_blueprint(blueprint)
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=os.getenv('DEFAULT_PORT'), help="Port of serving api")
+    parser.add_argument("-p", "--port", type=int, default=int(os.getenv('DEFAULT_PORT')), help="Port of serving api")
     args = parser.parse_args()
-    app.run(host="0.0.0.0", port=args.port , debug=True)
+    app.run(host="0.0.0.0", port=args.port)
     return app
 
 # print('hi')
