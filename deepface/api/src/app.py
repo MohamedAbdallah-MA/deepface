e6418c5b9a0f0cb38be9722ad989e0d392cf6964
Mohamed Abdallah <mohamed.abdallah5224@gmail.com>
2024-06-18 01:58:37 +0300

    try to solve the problem of deploying on railway

--- deepface/api/src/app.py
+++ deepface/api/src/app.py
@@ -10,14 +10,21 @@ def create_app():
     print(app.url_map)
     return app
 
-def create_online_app():
-    app = Flask(__name__)
-    app.register_blueprint(blueprint)
+if __name__ == "__main__":
+    deepface_app = create_app()
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=int(os.getenv('DEFAULT_PORT')), help="Port of serving api")
+    parser.add_argument("-p", "--port", type=int, default=os.getenv('DEFAULT_PORT'), help="Port of serving api")
     args = parser.parse_args()
-    app.run(host="0.0.0.0", port=args.port)
-    return app
+    deepface_app.run(host="0.0.0.0", port=args.port)
+
+# def create_online_app():
+#     app = Flask(__name__)
+#     app.register_blueprint(blueprint)
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("-p", "--port", type=int, default=int(os.getenv('DEFAULT_PORT')), help="Port of serving api")
+#     args = parser.parse_args()
+#     app.run(host="0.0.0.0", port=args.port)
+#     return app
 
 # print('hi')
 # print(blueprint)
