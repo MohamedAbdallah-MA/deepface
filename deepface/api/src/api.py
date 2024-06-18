5b060c418fc4e8162b14a7e65cc3dbe1708cdd01
Mohamed Abdallah <mohamed.abdallah5224@gmail.com>
2024-06-18 04:00:21 +0300

    revert most of previous commits

--- deepface/api/src/api.py
+++ deepface/api/src/api.py
@@ -5,6 +5,6 @@ import os
 if __name__ == "__main__":
     deepface_app = app.create_app()
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=os.getenv('DEFAULT_PORT'), help="Port of serving api")
+    parser.add_argument("-p", "--port", type=int, default=int(os.getenv('DEFAULT_PORT')), help="Port of serving api")
     args = parser.parse_args()
     deepface_app.run(host="0.0.0.0", port=args.port)
